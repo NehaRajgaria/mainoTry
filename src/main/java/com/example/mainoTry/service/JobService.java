dad0e1756a9a18fb3a23c39c05a636637f2eb588
NehaRajgaria <neha.rajgaria01@gmail.com>
2026-02-02 11:36:07 +0530

    Update JobService.java

--- src/main/java/com/example/mainoTry/service/JobService.java
+++ src/main/java/com/example/mainoTry/service/JobService.java
@@ -3,10 +3,12 @@ package com.example.mainoTry.service;
 import com.example.mainoTry.enums.JobStatus;
 import com.example.mainoTry.model.UploadJob;
 import com.example.mainoTry.model.VideoTask;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import java.util.*;
 import java.util.concurrent.*;
 
+@Slf4j
 @Service
 public class JobService {
 
@@ -28,14 +30,19 @@ public class JobService {
     }
 
     private void process(UploadJob job) {
+        log.info("Job {} processing started with {} videos.", job.jobId, job.videos.size());
         job.status = JobStatus.IN_PROGRESS;
 
         for (VideoTask task : job.videos) {
             int retryLimit = 3, retries = 0;
             while (retries < retryLimit) {
                 uploadService.upload(task);
-                if (task.success) break;
+                if (task.success) {
+                    log.info("Video {} uploaded successfully.", task.title);
+                    break;
+                }
                 retries++;
+                log.info("Error processing video {} in try number {}.", task.title,  retries);
             }
         }
 
